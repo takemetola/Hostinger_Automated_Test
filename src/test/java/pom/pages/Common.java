aaadd6ae3297d958c39901166f38066bef248d56
andriusjurkus <andrius.p.jurkus@gmail.com>
2024-01-18 17:55:57 +0200

    Added methods to TestBase class, Common class and TestListener

--- src/test/java/pom/pages/Common.java
+++ src/test/java/pom/pages/Common.java
@@ -1,4 +1,24 @@
 package pom.pages;
 
+import pom.utils.Driver;
+
+import java.time.Duration;
+
 public class Common {
+    public static void setUpChrome() {
+        Driver.setUpChromeDriver();
+    }
+
+    public static void openUrl(String url) {
+        Driver.getDriver().get(url);
+    }
+
+    public static void quitDriver() {
+        Driver.quitDriver();
+    }
+
+    public static void setUpChrome(int durationOfSeconds) {
+        Driver.setUpChromeDriver();
+        Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(durationOfSeconds));
+    }
 }
--- src/test/java/pom/utils/TestListener.java
+++ src/test/java/pom/utils/TestListener.java
@@ -1,4 +1,36 @@
 package pom.utils;
 
-public class TestListener {
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class TestListener implements ITestListener {
+    private void takeScreenshot() {
+        try {
+            TakesScreenshot screenshot = (TakesScreenshot) Driver.getDriver();
+            File screenshotFile = screenshot.getScreenshotAs(OutputType.FILE);
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH_mm_ss_SSS");
+            String dateTime = LocalDateTime.now().format(formatter);
+
+            String fileDirectory = "./screenshot/" + dateTime + "screenshot.png";
+
+            File file = new File(fileDirectory);
+            FileUtils.copyFile(screenshotFile, file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        takeScreenshot();
+    }
 }

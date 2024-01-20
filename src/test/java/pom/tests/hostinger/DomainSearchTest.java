b1dc70b0e6b974623740ec41698c2e2a72c0c08f
andriusjurkus <andrius.p.jurkus@gmail.com>
2024-01-20 15:04:30 +0200

    Added DomainSearchPage and DomainSearchTest classes

---
+++ src/test/java/pom/tests/hostinger/DomainSearchTest.java
@@ -0,0 +1,12 @@
+package pom.tests.hostinger;
+
+import org.testng.annotations.BeforeMethod;
+import pom.tests.TestBase;
+
+public class DomainSearchTest extends TestBase {
+    @BeforeMethod
+    @Override
+    public void setUp() {
+
+    }
+}

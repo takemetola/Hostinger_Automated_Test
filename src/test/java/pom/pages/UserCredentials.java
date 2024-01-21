8d55d76d35f7960677669aa578ce0affaccf9fcf
andriusjurkus <andrius.p.jurkus@gmail.com>
2024-01-22 00:28:55 +0200

    Added UserCredentials class to store email and password.

---
+++ src/test/java/pom/pages/UserCredentials.java
@@ -0,0 +1,15 @@
+package pom.pages;
+
+public class UserCredentials {
+    private static final String email = "desire2work4hostinger@gmail.com";
+    private static final String password = "Testas123!";
+
+    public static String getEmail() {
+        return email;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+}
+
